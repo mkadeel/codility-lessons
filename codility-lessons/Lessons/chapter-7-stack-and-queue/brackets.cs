33ea01aefe86623e08fe5d40778c8ff73034a1a3
mkhurramadeel <mkhurramadeel@gmail.com>
2020-11-21 16:53:35 +0500

    Add project files.

---
+++ codility-lessons/Lessons/chapter-7-stack-and-queue/brackets.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace codility_lessons.Lessons.chapter_7_stack_and_queue
+{
+    public class brackets
+    {
+        public int solution(string[] S)
+        {
+            Stack<char> braces = new Stack<char>();
+
+            char current;
+            for (int i = 0; i < S.Length; i++)
+            {
+                current = Convert.ToChar(S[i]);
+                switch (current)
+                {
+                    case '}':
+                        if (braces.Count == 0 || braces.Pop() != '{')
+                            return 0;
+                        break;
+                    case ']':
+                        if (braces.Count == 0 || braces.Pop() != '[')
+                            return 0;
+                        break;
+                    case ')':
+                        if (braces.Count == 0 || braces.Pop() != '(')
+                            return 0;
+                        break;
+                    default:
+                        braces.Push(current);
+                        break;
+                }
+            }
+            return braces.Count > 0 ? 0 : 1;
+        }
+    }
+}
