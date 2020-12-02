60073ba799497766fc8510f40fb0a45b9451ffd4
mkhurramadeel <mkhurramadeel@gmail.com>
2020-12-02 23:59:26 +0500

    .

--- codility-lessons/Program.cs
+++ codility-lessons/Program.cs
@@ -10,22 +10,6 @@ namespace codility_lessons
     {
         public static void Main(string[] args)
         {
-            int[] A = { 5, -7, 3, 5, -2, 4, -1 };
-            int max_slice, max_ending;
-            max_ending = max_slice = 0;
-            foreach(int a in A)
-            {
-                max_ending = max(0, max_ending + a);
-                max_slice= max(max_slice, max_ending);
-            }
-
-            Console.WriteLine( max_slice);
-
-        }
-
-        static int max(int a , int b)
-        {
-            return a < b ? b : a;
         }
     }
 }
